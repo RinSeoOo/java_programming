cb2431e21a76f7f5f91e6e244cbeef69e1489f4e
문서영 <msy042195@gmail.com>
2024-04-13 10:13:41 +0900

    rectangle_22에서 contains 내용 포함 (수정 필요)

--- Assignment_java/Rectangle_22.java
+++ Assignment_java/Rectangle_22.java
@@ -20,7 +20,17 @@ class Rectangle{
     }
 
     public boolean contains(Rectangle r){
-        
+        boolean tf = false;
+        if(r.square() < square()){
+            tf = true;
+        }
+        return tf;
+        /*
+         * 다음멤버를가지고직사각형을표현하는 Rectangle 클래스를작성하라.
+         * ●int 타입의 x, y, width, height 필드 : 사각형을구성하는점과크기정보 (x, y)는사각형의좌상단꼭지점의좌표로한다.
+         * ●x, y, width, height 값을매개변수로받아필드를초기화하는생성자
+         * ●boolean contains(Rectangle r) : 매개변수로받은 r이 현사각형안에있으면 true 리턴
+         */
     }
 }
 
@@ -30,7 +40,7 @@ public class Rectangle_22 {
         Rectangle s = new Rectangle(5, 5, 6, 6);
         Rectangle t = new Rectangle(1, 1, 10, 10);
 
-        r.sho();
+        r.show();
         System.out.println("Area : " + s.square());
         if(t.contains(r)) System.out.println("t includes r");
         if(t.contains(s)) System.out.println("t includes s");
